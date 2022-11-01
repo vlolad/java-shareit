7f3ff3e81c720334244bd23663525217af7fb03c
vlolad <vlad19.99@yandex.ru>
2022-11-02 02:13:10 +0300

    feature: improve constraint violations messages

--- src/main/java/ru/practicum/shareit/handler/ErrorHandler.java
+++ src/main/java/ru/practicum/shareit/handler/ErrorHandler.java
@@ -1,16 +1,23 @@
 package ru.practicum.shareit.handler;
 
 import lombok.extern.slf4j.Slf4j;
+import org.hibernate.validator.internal.engine.path.PathImpl;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import ru.practicum.shareit.handler.exception.NotFoundException;
+import ru.practicum.shareit.handler.model.ValidationErrorResponse;
+import ru.practicum.shareit.handler.model.Violation;
 import ru.practicum.shareit.item.exception.ItemBadRequestException;
 import ru.practicum.shareit.user.exception.UserCreationException;
 
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
 import javax.validation.ValidationException;
 import java.util.Locale;
 
@@ -47,10 +54,33 @@ public class ErrorHandler {
         return new ResponseEntity<>(e.getMessage(), e.getStatusCode());
     }
 
-    @ExceptionHandler
-    public ResponseEntity<String> handleMethodArgumentNotValidException(final MethodArgumentNotValidException e) {
-        log.error("Annotation validation exception: {}", e.getMessage());
-        return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+    @ExceptionHandler(ConstraintViolationException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ResponseBody
+    ValidationErrorResponse onConstraintValidationException(
+            ConstraintViolationException e) {
+        log.error("Handle ConstraintViolationException: {}", e.getMessage());
+        ValidationErrorResponse error = new ValidationErrorResponse();
+        for (ConstraintViolation violation : e.getConstraintViolations()) {
+            error.getViolations().add(
+                    new Violation(((PathImpl)violation.getPropertyPath()).getLeafNode().getName(),
+                            violation.getMessage()));
+        }
+        return error;
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ResponseBody
+    ValidationErrorResponse onMethodArgumentNotValidException(
+            MethodArgumentNotValidException e) {
+        log.error("Handle MethodArgumentNotValidException: {}", e.getMessage());
+        ValidationErrorResponse error = new ValidationErrorResponse();
+        for (FieldError fieldError : e.getBindingResult().getFieldErrors()) {
+            error.getViolations().add(
+                    new Violation(fieldError.getField(), fieldError.getDefaultMessage()));
+        }
+        return error;
     }
 
     @ExceptionHandler
