f6aa454660b532538e6383821e43dea4df87ed8c
vlolad <vlad19.99@yandex.ru>
2022-11-19 02:05:07 +0300

    add booking model

--- src/main/java/ru/practicum/shareit/booking/Booking.java
+++ src/main/java/ru/practicum/shareit/booking/Booking.java
@@ -1,7 +1,22 @@
 package ru.practicum.shareit.booking;
 
-/**
- * TODO Sprint add-bookings.
- */
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import ru.practicum.shareit.item.model.Item;
+import ru.practicum.shareit.user.User;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Booking {
+
+    private Integer id;
+    private LocalDateTime start;
+    private LocalDateTime end;
+    private Item item;
+    private User booker;
+    private BookingStatus status;
 }
