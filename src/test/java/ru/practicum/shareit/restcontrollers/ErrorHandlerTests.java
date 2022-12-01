ef60939aed36ad2dbf0e2335ae32327c0a1f1862
vlolad <vlad19.99@yandex.ru>
2022-12-01 23:16:04 +0300

    try to breach tests

--- src/test/java/ru/practicum/shareit/restcontrollers/ErrorHandlerTests.java
+++ src/test/java/ru/practicum/shareit/restcontrollers/ErrorHandlerTests.java
@@ -3,12 +3,15 @@ package ru.practicum.shareit.restcontrollers;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
 import ru.practicum.shareit.handler.ErrorHandler;
+import ru.practicum.shareit.item.exception.BadCommentException;
 
 import javax.validation.ValidationException;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 public class ErrorHandlerTests {
@@ -21,5 +24,12 @@ public class ErrorHandlerTests {
         ValidationException exc = new ValidationException("Message");
         String result = errorHandler.handleValidationException(exc);
         assertThat(result, equalTo(exc.getMessage()));
+    }
+
+    @Test
+    void testBadCommentException() {
+        BadCommentException exc = new BadCommentException("Message");
+        ResponseEntity<String> result = errorHandler.handleCommentNotTrueException(exc);
+        assertTrue(result.toString().contains("Message"));
     }
 }
